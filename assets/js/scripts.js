30e194fd889eba8197c00bd31aa4b923bfb3432a
EugeniaParedes <eugenia.paredes182@gmail.com>
2021-08-07 13:35:22 -0300

    comienzo seccion categorias

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,65 @@
-var getStorage = function () {
-    var locStor = JSON.parse(localStorage.getItem('ahorradas-data'));
-    if (!locStor) {
+type LocalStorage = {
+    categories?: Category[],
+    operations?: Operation[]
+}
+
+type Operation = {
+    description: string,
+    categories: string,
+    date: string,
+    amount: number,
+    type: "gasto" | "ganancia"
+}
+
+type Category = {
+    id: number,
+    name: string
+}
+
+const getStorage = (): LocalStorage => {
+    let locStor: LocalStorage = JSON.parse(localStorage.getItem('ahorradas-data'));
+
+    if(!locStor) {
         locStor = {
             categories: [
                 {
                     id: 1,
-                    name: "Comida"
+                    name: "Comida",
                 },
                 {
                     id: 2,
                     name: "Servicios"
-                }
+                },
+                {
+                    id: 3,
+                    name: "Salidas"
+                },
+                {
+                    id: 4,
+                    name: "Educación"
+                },
+                {
+                    id: 5,
+                    name: "Transporte"
+                },
+                {
+                    id: 4,
+                    name: "Trabajo"
+                },
             ],
             operations: []
-        };
+        } 
     }
     return locStor;
-};
-var init = function () {
-    localStorage.setItem('ahorradas-data', JSON.stringify(getStorage()));
-};
+}
+
+const formCategory = document.getElementById('form-category')
+formCategory.addEventListener('submit',()=> {
+    alert("funciona")
+})
+
+
+const init = () => {
+    localStorage.setItem('ahorradas-data', JSON.stringify(getStorage()))
+}
 init();
