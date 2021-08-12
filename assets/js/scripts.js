fb31ec812ae0cf29cf56f933f1c5f1f0acb19053
ayelencristi <ayelendcristi@gmail.com>
2021-08-12 20:11:09 -0300

    funcion mostrar/ocultar pestaña nueva operacion

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,3 +12,19 @@ var init = function () {
     localStorage.setItem('ahorradas-data', JSON.stringify(getStorage()));
 };
 init();
+// ------------ BOTON NUEVA OPERACION-----------
+var btnNewOperation = document.getElementById('new-operation');
+var divNewOperation = document.getElementById('div-new-operation');
+var btnAddCategory = document.getElementById('btn-add-category');
+var btnCancel = document.getElementById('btn-cancel');
+var divMain = document.getElementById('div-main');
+var tabNewOperation = function () {
+    divMain.classList.add('d-none');
+    divNewOperation.classList.remove('d-none');
+};
+btnNewOperation.addEventListener('click', tabNewOperation);
+var closeTabNewOperation = function () {
+    divMain.classList.remove('d-none');
+    divNewOperation.classList.add('d-none');
+};
+btnCancel.addEventListener('click', closeTabNewOperation);
